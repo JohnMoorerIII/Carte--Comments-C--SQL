e493b5cf1c9c60a973e5990fcae21afb7277e9b7
John Moorer <109092701+JohnMoorerIII@users.noreply.github.com>
2024-01-22 09:21:00 -0800

    Add files via upload

---
+++ .NET/Requests/CommentAddRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Requests.Comments
+{
+    public class CommentAddRequest
+    {
+        [MinLength(1)]
+        [MaxLength(50)]
+        public string Subject { get; set; }
+        
+        [Required]
+        [MinLength(1)]
+        [MaxLength(3000)]
+        public string Text { get; set; }
+        
+        public int ParentId { get; set; }
+
+        public int EntityTypeId { get; set; }
+    
+        public int EntityId { get; set; }
+        
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}
